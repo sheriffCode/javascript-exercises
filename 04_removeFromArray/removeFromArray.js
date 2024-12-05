a1cfc26466d6b33e3deea3365e043b14f3a7c63b
sheriffCode <151257645+sheriffCode@users.noreply.github.com>
2024-12-05 18:17:57 +0000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, items) {
+    const args = [...arguments].slice(1);
+    let result = array;
+    for (const item of args) {
+       result =  result.filter(el => el !== item);
+    }
+    return result;
 };
 
 // Do not edit below this line

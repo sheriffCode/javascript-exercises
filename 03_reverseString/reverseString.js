74ea12420ee1f7d81cfc7a203c8390d74def95d9
sheriffCode <151257645+sheriffCode@users.noreply.github.com>
2024-12-05 15:11:58 +0000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const revString = string.split("").reverse().join("");
+    return revString;
 };
 
 // Do not edit below this line

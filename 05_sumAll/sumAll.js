9995ddfacc19d9f93bfbe8a2d295d17c42d29cbe
sheriffCode <151257645+sheriffCode@users.noreply.github.com>
2024-12-07 14:32:46 +0000

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(Number.isInteger(int1) && Number.isInteger(int2) &&
+        int1 >= 0 && int2 >= 0) {
+        const sorted = [...arguments].sort((a, b) => a - b);
+        let sum = 0;
+        for(let i = sorted[0]; i <= sorted[1]; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
